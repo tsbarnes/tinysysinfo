a8f0cb29fcd0859265521c214a36ab80f4ea7a2f
Thea Barnes <thea@tsbarnes.com>
2021-10-08 18:20:00 -0400

    Restructuring

--- tinysysinfo/__main__.py
+++ tinysysinfo/__main__.py
@@ -33,7 +33,7 @@ class TinySysInfo:
         signal.signal(signal.SIGINT, self.shutdown)
         signal.signal(signal.SIGTERM, self.shutdown)
 
-    def shutdown(self):
+    def shutdown(self, *args):
         self.logger.info("tinysysinfo shutting down gracefully...")
         self.display.clear()
         time.sleep(1)
@@ -53,7 +53,7 @@ class TinySysInfo:
 
             filename = getframeinfo(currentframe()).filename
             parent = Path(filename).resolve().parent
-            logo = Image.open(os.path.join(parent, 'raspberry-pi.png'))
+            logo = Image.open(os.path.join(parent, '../raspberry-pi.png'))
             image.paste(logo, box=(50, 90))
 
             with open('/sys/firmware/devicetree/base/model', 'r') as model_file:
